5b92dfae37c44df330207a8ba666a9717f8d93fe
DiegoPalmieri <palmieribarretodiego@gmail.com>
2023-10-06 15:16:19 -0500

    Creacion de la tabla 'peticion' donde el admin recibirá las reservas

--- src/app/components/usuarios/admin/peticiones/peticiones.component.ts
+++ src/app/components/usuarios/admin/peticiones/peticiones.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-peticiones',
   templateUrl: './peticiones.component.html',
-  styleUrls: ['./peticiones.component.css']
+  styleUrls: ['./peticiones.component.css'],
 })
 export class PeticionesComponent {
+  dataSource!: MatTableDataSource<any>;
 
+  displayedColumns = [
+    {
+      profesor: 'Andres Carmona',
+      horaInicio: '3:13 p.m',
+      horaFin: '5:30 pm',
+      fecha: '6/10/2023',
+      recurso: 'VIDEO BIN',
+      acciones: ''
+    },
+  ];
 }
