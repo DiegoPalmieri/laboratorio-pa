648b98345b59df4eb51f5e44e65edca874414621
DiegoPalmieri <palmieribarretodiego@gmail.com>
2023-10-09 09:45:20 -0500

    seteo de datos en la tabla peticion, y diseño en el mismo

--- src/app/components/usuarios/admin/peticiones/peticiones.component.ts
+++ src/app/components/usuarios/admin/peticiones/peticiones.component.ts
@@ -15,14 +15,72 @@ export class PeticionesComponent implements OnInit {
     'horaFin',
     'fecha',
     'recurso',
+    'acciones'
   ];
+
+  tablaInfo: any []=[
+
+    {
+      profesor: 'HORACIO HERNANDEZ',
+      horaInicio: '8:27 a.m.',
+      horaFin: '10:00 p.m.',
+      fecha: '9/10/2023',
+      recurso: 'VIDEO BIN'
+
+    },
+
+    {
+      profesor: 'MARIA DE LA CRUZ',
+      horaInicio: '8:27 a.m.',
+      horaFin: '10:00 p.m.',
+      fecha: '9/10/2023',
+      recurso: 'VIDEO BIN'
+
+    },
+
+    {
+      profesor: 'DIEGO PALMIERI',
+      horaInicio: '8:45 a.m.',
+      horaFin: '10:12 p.m.',
+      fecha: '9/10/2023',
+      recurso: 'LAPTOP'
+
+    },
+
+    {
+      profesor: 'HORACIO HERNANDEZ',
+      horaInicio: '8:43 a.m.',
+      horaFin: '10:34 p.m.',
+      fecha: '9/10/2023',
+      recurso: 'VGA'
+
+    },
+    {
+      profesor: 'ANDRES ESCOBAR',
+      horaInicio: '8:54 a.m.',
+      horaFin: '10:34 p.m.',
+      fecha: '9/10/2023',
+      recurso: 'HDMI',
+
+    }
+
+  ]
 
   ngOnInit(): void {
     this.getData();
   }
 
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
   getData() {
-    this.dataSource = new MatTableDataSource(this.displayedColumns);
-    console.log(this.dataSource);
+    this.dataSource = new MatTableDataSource(this.tablaInfo);
+    console.table(this.tablaInfo);
   }
 }
